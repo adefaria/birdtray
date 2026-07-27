a281b90263d6c3316354111019c7e9cb5b311fa1
Andrew DeFaria <Andrew@DeFaria.com>
2026-07-27 11:01:59 -0700

    toggle_birdtray.sh no longer needed

--- toggle_birdtray.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Toggle Thunderbird visibility using Birdtray's command line interface
-# This script assumes "birdtray" is in the PATH or installed to /usr/local/bin
-exec birdtray -t "$@"
